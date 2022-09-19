c6f01aaea5a56200439c1bba62325b278055ab1a
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-09-20 00:55:26 +0300

    adding abstract class for two models

--- app/core/db.py
+++ app/core/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Boolean, Column, DateTime, Integer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
 
@@ -15,6 +15,15 @@ class PreBase:
 
 
 Base = declarative_base(cls=PreBase)
+
+class CommonProjectDonation(Base):
+    __abstract__ = True
+    full_amount = Column(Integer) # больше 0
+    invested_amount = Column(Integer, default=0) # значение по умолчанию — 0
+    fully_invested = Column(Boolean, default=False) #  значение по умолчанию — False
+    create_date = Column(DateTime) # должно добавляться автоматически в момент создания проекта/поступления пожертвования
+    close_date = Column(DateTime) # проставляется автоматически в момент набора нужной суммы/когда вся сумма пожертвования была распределена по проектам
+
 
 engine = create_async_engine(settings.database_url)
 
