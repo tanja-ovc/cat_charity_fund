b94658024bf9e2c842bbe16e67171f74511c6a3a
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-09-21 01:34:19 +0300

    updated routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router
+from app.api.endpoints import user_router, charityproject_router
 
 main_router = APIRouter()
 main_router.include_router(
-    ..., prefix='/...', tags=['...']
+    charityproject_router, prefix='/charity_project', tags=['Charity Projects']
 )
-main_router.include_router(
-    ..., prefix='/...', tags=['...s']
-)
+# main_router.include_router(
+#     ..., prefix='/...', tags=['...']
+# )
 main_router.include_router(user_router)
