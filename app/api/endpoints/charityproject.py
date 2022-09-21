a8ad84012560dc102cfc7d7269aacaa42b9c08ad
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-09-22 01:04:44 +0300

    more CRUD functions added

--- app/api/endpoints/charityproject.py
+++ app/api/endpoints/charityproject.py
@@ -1,19 +1,23 @@
-
-from datetime import datetime
+from typing import List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import (
+    check_charityproject_not_closed,
+    check_charityproject_exists,
+    check_charityproject_investment_exists,
+    check_full_amount_not_less_than_before,
+    check_name_duplicate
+)
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud import charityproject_crud
-from app.schemas import CharityProjectCreate, CharityProjectDB
+from app.schemas import (
+    CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
+)
 
 router = APIRouter()
-
-
-async def insert_current_time() -> datetime:
-        return datetime.now()
 
 
 @router.post(
@@ -27,6 +31,64 @@ async def create_new_charityproject(
         session: AsyncSession = Depends(get_async_session),
 ):
     """Только для суперюзеров."""
-    # await check_name_duplicate(meeting_room.name, session) # ?
+    await check_name_duplicate(charityproject.name, session)
     new_charityproject = await charityproject_crud.create(charityproject, session)
     return new_charityproject
+
+
+@router.get(
+    '/',
+    response_model=List[CharityProjectDB],
+    response_model_exclude_none=True,
+)
+async def get_all_charityprojects(
+    session: AsyncSession = Depends(get_async_session)
+):
+    charityprojects_list = await charityproject_crud.get_multi(session)
+    return charityprojects_list
+
+
+@router.delete(
+    '/{charityproject_id}',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
+)
+async def remove_charityproject(
+        charityproject_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    """Только для суперюзеров."""
+    charityproject_db = await check_charityproject_exists(
+        charityproject_id, session
+    )
+    await check_charityproject_investment_exists(charityproject_db)
+    charityproject = await charityproject_crud.remove(
+        charityproject_db, session
+    )
+    return charityproject
+
+
+@router.patch(
+    '/{charityproject_id}',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
+)
+async def partially_update_charityproject(
+        charityproject_id: int,
+        obj_in: CharityProjectUpdate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    """Только для суперюзеров."""
+    charityproject_db = await check_charityproject_exists(
+        charityproject_id, session
+    )
+    if obj_in.name is not None:
+        await check_name_duplicate(obj_in.name, session)
+    await check_charityproject_not_closed(charityproject_db)
+    await check_full_amount_not_less_than_before(charityproject_db, obj_in)
+    charityproject = await charityproject_crud.update(
+        charityproject_db, obj_in, session
+    )
+    return charityproject
