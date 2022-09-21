4a1dbd86ab4523eed044c5bd1645e6b09ba9869a
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-09-22 01:06:10 +0300

    get_charityproject_id_by_name function added to CRUDCharityProject class

--- app/crud/charityproject.py
+++ app/crud/charityproject.py
@@ -1,14 +1,26 @@
-# from typing import Optional
+from typing import Optional
 
-# from sqlalchemy import select
-# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
 from app.models import CharityProject
 
 
 class CRUDCharityProject(CRUDBase):
-    pass
+    
+    async def get_charityproject_id_by_name(
+            self,
+            charityproject_name: str,
+            session: AsyncSession,
+    ) -> Optional[int]:
+        charityproject_id = await session.execute(
+            select(CharityProject.id).where(
+                CharityProject.name == charityproject_name
+            )
+        )
+        db_charityproject_id = charityproject_id.scalars().first()
+        return db_charityproject_id
 
 
 charityproject_crud = CRUDCharityProject(CharityProject)
